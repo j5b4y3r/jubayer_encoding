a7f20d8fd9abfb527a7d5b963d65b62502b5e89d
j5b4y3r <jubayer.yunusn1@gmail.com>
2024-02-15 00:53:31 +0600

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jubayer_encoding',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     description='A package for encoding and decoding strings',
     author='Jubayer',
